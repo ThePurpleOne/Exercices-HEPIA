4f496c345d31611ab7ae5e6a7c5938106569c853
jonas@stirnemann.xyz <tpo@DESKTOP-033MUEM.localdomain>
2020-10-25 14:31:58 +0100

    Creation du projet pour l'exercice 4 et Makefile

---
+++ EX4_TableauUni/headers/tableau.h
@@ -0,0 +1,14 @@
+#ifndef _TABLEAU_H_
+#define _TABLEAU_H_
+
+
+//http://malaspinas.academy/
+
+//############## Variables Globales et structs ##############
+
+
+//############## FONCTIONS PROTOTYPES ##############
+
+//############## END FONCTIONS PROTOTYPES ##############
+
+#endif
---
+++ EX4_TableauUni/src/main.c
@@ -0,0 +1,17 @@
+/*===========================================================================*
+   Auteur   : STIRNEMANN Jonas
+   Date création : 25/10/2020
+=============================================================================
+   Descriptif: Gestion de tableaux Unidimensionnels
+=*===========================================================================*/
+
+#include <stdio.h>
+#include <math.h>
+#include "../headers/tableau.h"
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello World!\n");
+	return 0;
+}
+
