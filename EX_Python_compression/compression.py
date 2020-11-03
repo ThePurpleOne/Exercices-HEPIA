808c881d3b671942b596c8c6eaff03c734fb21db
Jonas STIRNEMANN <ThePurpleOne@users.noreply.github.com>
2020-11-03 21:38:45 +0100

    Modif EX_Python_compression: ajout fonction sorting alphabet et debut sorting occurence

--- EX_Python_compression/compression.py
+++ EX_Python_compression/compression.py
@@ -7,21 +7,62 @@
 
 # coding: utf-8
 
-ENCODING_FORMAT = 'UTF-8'
-
-with open("inputFile.txt", 'r', encoding=ENCODING_FORMAT) as inputFile:
-	textInput = inputFile.read()
-
-print(textInput)
-
-textOutput = ''.join(sorted(textInput))
-
-for car in textOutput:
-	print(f"Car : {car}  Hex value : {hex(ord(car))}")
 
 
-with open("outputText.txt", 'w', encoding=ENCODING_FORMAT) as outputText:
-	outputText.write(textOutput)
+# with open("inputFile.txt", 'r', encoding=ENCODING_FORMAT) as inputFile:
+# 	textInput = inputFile.read()
+
+# print(textInput)
+
+# textOutput = ''.join(sorted(textInput))
+
+# for car in textOutput:
+# 	print(f"Car : {car}  Hex value : {hex(ord(car))}")
+
+
+# with open("outputText.txt", 'w', encoding=ENCODING_FORMAT) as outputText:
+# 	outputText.write(textOutput)
 
 
 
+def createAlphabeticallySortedFileFromFile(fileIn):
+	"""Sort a file in Alphabeticall order and put it in an other file
+
+	Args:
+		file (string): file name of the file you want sorted
+	"""
+	ENCODING_FORMAT = 'UTF-8'
+	with open(fileIn, 'r', encoding=ENCODING_FORMAT) as inputFile:
+		textInput = inputFile.read()
+  
+	textOutput = ''.join(sorted(textInput))
+
+	with open("alphabeticallySorted" + fileIn, 'w', encoding=ENCODING_FORMAT) as outputText:
+		outputText.write(textOutput)
+
+def createOccurenceSortedFileFromFile(fileIn):
+	"""Sort a file in Number of occurence and put it in an other file
+
+	Args:
+		file (string): file name of the file you want sorted
+	"""
+	from collections import Counter
+
+	ENCODING_FORMAT = 'UTF-8'
+	with open(fileIn, 'r', encoding=ENCODING_FORMAT) as inputFile:
+		textInput = inputFile.read()
+
+
+	# textOutput = [item for items, c in Counter(ini_list).most_common() for item in [items] * c]
+	textOutput = list(textInput)
+ 	#Counter(textOutput).most_common() returns a dictionnary of caracters sorted by occurence
+	for car in Counter(textOutput).most_common(): 
+		for item in []
+		print(car * car[][])
+	
+	# with open("OccurenceSorted" + fileIn, 'w', encoding=ENCODING_FORMAT) as outputText:
+	# 	outputText.write(textOutput)
+
+
+# createAlphabeticallySortedFileFromFile("inputFile.txt")
+createOccurenceSortedFileFromFile("inputFile.txt")
